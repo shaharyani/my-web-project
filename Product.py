ee2d47ca2167f9954fd86d65c1b21dfbed2cb705
shaharyani <shahryani96@gmail.com>
2025-12-29 19:10:04 +0200

    Added the base of the Product saving and reading from the DB

--- Product.py
+++ Product.py
@@ -1,18 +1,28 @@
 class Product:
-    def __init__(self, serial: str, code:str, card_number:int, land_number:str):
+    def __init__(self, serial: str, code:str, card_type:str, land_type:str, status:str, owner:str):
         self.serial = serial
         self.code = code
-        self.card_number = card_number
-        self.land_number = land_number
+        self.card_type = card_type
+        self.land_type = land_type
+        self.status = status  # R - RED | B - BLACK | W - WHITE | N - NONE
+        self.owner = owner
 
-    def getProductSerial(self):
+    def get_owner(self):
+        return self.owner
+
+    def get_serial(self):
         return self.serial
 
-    def getProductCode(self):
+    def get_code(self):
         return self.code
 
-    def getProductCardNumber(self):
-        return self.card_number
+    def getCardType(self):
+        return self.card_type
 
-    def getProductLandNumber(self):
-        return self.land_number
+    def getLandType(self):
+        return self.land_type
+
+    def __str__(self):
+        return (f"Product(serial='{self.serial}', code='{self.code}', "
+                f"card_type='{self.card_type}', land_type='{self.land_type}', "
+                f"status='{self.status}', owner='{self.owner}')")
